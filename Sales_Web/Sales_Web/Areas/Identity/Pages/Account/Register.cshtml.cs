d400e7fa3f7f07393f8e31b0c73f474ad97e6349
EmKhongLaNangTher <148929364+EmKhongLaNangTher@users.noreply.github.com>
2024-04-04 13:05:32 +0700

    update Role

--- Sales_Web/Sales_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Sales_Web/Sales_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -79,18 +79,12 @@ namespace Sales_Web.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            ReturnUrl = returnUrl;
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        }
-
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
             if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-            
+
             }
 
             Input = new()
@@ -101,6 +95,13 @@ namespace Sales_Web.Areas.Identity.Pages.Account
                     Value = i
                 })
             };
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+           
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
