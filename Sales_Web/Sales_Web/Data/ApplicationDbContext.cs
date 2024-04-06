7f89385bddb025d7448a9eca7249d7aa40146e0f
dauhoangdien <dauhoangdien@gmail.com>
2024-04-06 11:41:49 +0700

    Dien clone commit

--- Sales_Web/Sales_Web/Data/ApplicationDbContext.cs
+++ Sales_Web/Sales_Web/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Sales_Web.Areas.Admin.Models.Accounts;
 using Sales_Web.Areas.Admin.Models.Employees;
 using Sales_Web.Models.Products;
 
 namespace Sales_Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
