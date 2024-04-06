a815f4415cc6ed89527ea1101a3b472d079f6454
EmKhongLaNangTher <148929364+EmKhongLaNangTher@users.noreply.github.com>
2024-04-06 17:32:33 +0700

    Shopping but still Error

---
+++ Sales_Web/Sales_Web/Areas/User/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Sales_Web.Areas.Admin.Models.Accounts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sales_Web.Areas.User.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public string ShippingAddress { get; set; }
+        public string Notes { get; set; }
+
+        [ForeignKey("UserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
--- Sales_Web/Sales_Web/Data/ApplicationDbContext.cs
+++ Sales_Web/Sales_Web/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using Sales_Web.Areas.Admin.Models.Employees;
 using Sales_Web.Areas.Admin.Models.Products;
 using Sales_Web.Models;
 using Sales_Web.Areas.Admin.Models.Accounts;
+using Sales_Web.Areas.User.Models;
 namespace Sales_Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -20,6 +21,9 @@ namespace Sales_Web.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
         //Account
     }
